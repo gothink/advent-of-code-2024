507efc1497df858a3e01562812dcf7c7ee089384
Zachary Goth <zach@gothink.dev>
2024-12-07 17:53:01 -0500

    day 7 part 2 complete

--- day7/day7.go
+++ day7/day7.go
@@ -10,14 +10,31 @@ import (
 	"sync"
 )
 
-func findResult(nums []uint64, oppMask int) (result uint64) {
+func findResult(nums []uint64, opMask int) (result uint64) {
 	result += nums[0]
 
 	for i, n := range nums[1:] {
-		if oppMask&(1<<i) == 0 {
+		if opMask&(1<<i) == 0 {
 			result += n
 		} else {
 			result *= n
+		}
+	}
+
+	return
+}
+
+func findResPart2(nums []uint64, opMap []int) (result uint64) {
+	result += nums[0]
+
+	for i, n := range nums[1:] {
+		switch opMap[i] {
+		case 0:
+			result += n
+		case 1:
+			result *= n
+		case 2:
+			result, _ = strconv.ParseUint(strconv.FormatUint(result, 10)+strconv.FormatUint(n, 10), 10, 64)
 		}
 	}
 
@@ -52,9 +69,29 @@ func solve(goal uint64, nums []uint64) (r uint64) {
 	return
 }
 
+func solvePart2(goal uint64, nums []uint64) (r uint64) {
+	numOps := len(nums) - 1
+	opMap := make([]int, numOps)
+	for i := range int(math.Pow(3, float64(numOps))) {
+		num := i
+		for j := 0; j < numOps; j++ {
+			opMap[j] = num % 3
+			num /= 3
+		}
+		if result := findResPart2(nums, opMap); result == goal {
+			r = result
+			break
+		}
+	}
+
+	return
+}
+
 func Day7() {
 	file, _ := os.Open("input/7.txt")
 	defer file.Close()
+
+	part1 := false
 
 	var wg sync.WaitGroup
 	results := make(chan uint64)
@@ -70,7 +107,12 @@ func Day7() {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			result := solve(goal, append([]uint64{}, nums...))
+			var result uint64
+			if part1 {
+				result = solve(goal, append([]uint64{}, nums...))
+			} else {
+				result = solvePart2(goal, append([]uint64{}, nums...))
+			}
 			results <- result
 		}()
 	}
