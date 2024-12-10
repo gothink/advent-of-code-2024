920f7953661a928065fee8b58c754e906e274f0b
Zachary Goth <zach@gothink.dev>
2024-12-09 19:08:41 -0500

    day 9 part 2 complete

--- day9/day9.go
+++ day9/day9.go
@@ -8,6 +8,8 @@ import (
 
 var fileSizes = []int{}
 var freeSizes = []int{}
+var blockOffsets = []int{}
+var blockStarts = []int{}
 var diskSize = 0
 var checksum = 0
 var currentBlock = 0
@@ -23,24 +25,7 @@ func calcChecksum(fileID, numBlocks int) (done bool) {
 	return
 }
 
-func parseLine(line string) {
-	for i := 0; i < len(line); i += 2 {
-		if fileSize, err := strconv.Atoi(string(line[i])); err == nil {
-			diskSize += fileSize
-			fileSizes = append(fileSizes, fileSize)
-		}
-
-		space := 0
-		if i+1 < len(line) {
-			freeSize, err := strconv.Atoi(string(line[i+1]))
-			if err != nil {
-				continue
-			}
-			space = freeSize
-		}
-		freeSizes = append(freeSizes, space)
-	}
-
+func solvePart1() {
 	currentID := len(fileSizes) - 1
 	fileSize := fileSizes[currentID]
 
@@ -81,17 +66,84 @@ outer:
 	}
 }
 
+func calcChecksum2(fileID, numBlocks, startBlock int) {
+	for i := startBlock; i < startBlock+numBlocks; i++ {
+		checksum += i * fileID
+	}
+}
+
+func solvePart2() {
+	for i := len(fileSizes) - 1; i >= 0; i-- {
+		hasMoved := false
+		for j := 0; j < i; j++ {
+			if freeSizes[j] >= fileSizes[i] {
+				freeSizes[j] -= fileSizes[i]
+				calcChecksum2(i, fileSizes[i], blockOffsets[j])
+				blockOffsets[j] += fileSizes[i]
+				blockOffsets[i] -= fileSizes[i]
+				hasMoved = true
+				break
+			}
+		}
+
+		if !hasMoved {
+			calcChecksum2(i, fileSizes[i], blockStarts[i])
+		}
+	}
+}
+
+func parseLine(line string) {
+	blockOffset, blockStart := 0, 0
+
+	for i := 0; i < len(line); i += 2 {
+		fileSize, err := strconv.Atoi(string(line[i]))
+		if err != nil {
+			continue
+		}
+
+		diskSize += fileSize
+		fileSizes = append(fileSizes, fileSize)
+
+		freeSize := 0
+		if i+1 < len(line) {
+			freeSize, err = strconv.Atoi(string(line[i+1]))
+			if err != nil {
+				continue
+			}
+		}
+
+		freeSizes = append(freeSizes, freeSize)
+
+		// part 2
+		blockOffset += fileSize
+		blockOffsets = append(blockOffsets, blockOffset)
+		blockOffset += freeSize
+
+		blockStarts = append(blockStarts, blockStart)
+		blockStart += fileSize + freeSize
+	}
+}
+
 func Day9() {
 	fp, err := utils.Fetch("9")
 	if err != nil {
 		return
 	}
+
 	lines := make(chan string)
-	// fp := "input/test/9.txt"
 	go utils.Scan(fp, lines)
 
 	for line := range lines {
 		parseLine(line)
 	}
-	fmt.Println("Checksum: ", checksum)
+
+	part := 2
+	switch part {
+	case 1:
+		solvePart1()
+		fmt.Println("Checksum part 1: ", checksum)
+	case 2:
+		solvePart2()
+		fmt.Println("Checksum part 2: ", checksum)
+	}
 }
